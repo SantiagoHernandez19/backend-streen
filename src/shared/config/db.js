34a313e3c936363e28aa2a148e57540515167fb5
streetcaps511-a11y <streetcaps511@gmail.com>
2026-03-23 13:33:17 -0500

    Haciendo feliz a Render con su conexion favorita

--- src/shared/config/db.js
+++ src/shared/config/db.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg');
 
 // Usamos el DATABASE_URL que Render nos da (o el local)
 const pool = new Pool({
-  connectionString: "postgres://streen_user:hXv6kR9v9u9h8y7t6r5e4w3q2a1s0d9f@dpg-cuj8k8568j0s7392j0g0-a.oregon-postgres.render.com/streen",
+  // Prioriza la conexión interna de Render (DATABASE_URL), si no existe usa la externa
+  connectionString: process.env.DATABASE_URL || "postgres://streen_user:hXv6kR9v9u9h8y7t6r5e4w3q2a1s0d9f@dpg-cuj8k8568j0s7392j0g0-a.oregon-postgres.render.com/streen",
   ssl: {
     rejectUnauthorized: false
   }
